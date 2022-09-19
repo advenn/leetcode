0bfc0a5c34ab1da25bb70007a8be429eedff2331
advenuz <qurbonovasadbek@gmail.com>
2022-09-19 13:38:50 +0500

    longest palindrome

---
+++ longestPalindrome.py
@@ -0,0 +1,93 @@
+import time
+start = time.time()
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        """
+        Input: s = "babad"
+        Output: "bab"
+        Explanation: "aba" is also a valid answer.
+        ___________
+        Input: s = "cbbd"
+        Output: "bb"
+        ___________
+        :param s:
+        :return:
+        """
+        # def isPalindrome(s):
+        #     return s == s[::-1]
+        def isPalindrome(s: str) -> bool:
+            if not s:
+                return False
+            return s == s[::-1]
+            # length = len(s) - 1
+            # first = 0
+            # mid = length // 2
+            # while True:
+            #     if s[first] == s[length]:
+            #         first += 1
+            #         length -= 1
+            #     else:
+            #         return False
+            #     if first > mid:
+            #         return True
+        if isPalindrome(s):
+            return s
+        length = len(s)
+        first = 0
+        longest = ""
+        while True:
+            string = s[first:length]
+            secleng = len(string)
+            second = 0
+            spider = 0
+            while True:
+                cut = string[second:second + spider]
+                if not string[second:second + spider]:
+                    spider += 1
+                    continue
+                # print(cut, longest)
+                if isPalindrome(cut) and len(longest) < len(cut):
+                    longest = cut
+                spider += 1
+                if spider > secleng:
+                    break
+            first += 1
+            if first >= length:
+                return longest
+
+
+s = Solution()
+# print(s.longestPalindrome("pcagacp"))
+print(s.longestPalindrome("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+print(time.time() - start)
+print(s.longestPalindrome("pihoigwlvzvtrugdolvtzrkyelaqdvbijzmkhebzawboaxkdjyfocpewwztffuaibcqurwwmijmvrnpfcoglyxpxkrbhupoxcafabxtoecodsjgngrionuvzaiigevuvruxxiwpjzjlqgenglhprcgzgpdzabrjhkbtfrbmwpcszepxhwiwdhvnpsmhhaiqsbeiwsaeomqtzcpjzfknejxlxwtpkufanhuoyjgihdzhtxnyctazzvnttjspfztjurdwmmzrvobcatkorfhpieoqfetcglembkgbexsznuduhrfoxkbswkanqwfkoktnnujqetijaqhrxuhkgsezfdrncbaltctwcourdbpdwhqlsxfwsoaduaqkbjeekwwykptjthhtokrvzsuelsywyznqscnwiszogzqvfsgggzltlvzkllinpfaigswquqfvabbzvestwxhbnfjhnvfhyxalchmtkcwnyyrbwjsoqooypwteozbivqiyldpqlykxinmzkgnmfbobgjivlzubfen"))
+# print(isPalindrome("aavttvaa"))
+
+# def isPalindrome(s: str) -> bool:
+#     length = len(s) - 1
+#     first = 0
+#     mid = length // 2
+#     while True:
+#         if s[first] == s[length]:
+#             first += 1
+#             length -= 1
+#         else:
+#             return False
+#         if first > mid:
+#             return True
+# def isPalindrome( x: int) -> bool:
+#     temp = x
+#     reverse = 0
+#     while x > 0:
+#         digit = x % 10
+#         reverse = reverse * 10 + digit
+#         x = x // 10
+#     if temp == reverse:
+#         return True
+#     return False
+
+print(time.time()-start)
+
+# class Solution:
+#     def longestPalindrome(self, s: str) -> str:
+
