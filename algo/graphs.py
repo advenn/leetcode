f9d0149437528f193df3c4fc60da65de189cff6a
advenuz <qurbonovasadbek@gmail.com>
2022-10-05 17:10:50 +0500

    plusOne, graphs

---
+++ algo/graphs.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+# search_queue = deque()
+graph = {
+    "you": ["alice", "bob", "claire"],
+    "bob": ["anuj", "peggy"],
+    "alice": ["peggy"],
+    "claire": ["thom", "jonny"],
+    "anuj": [],
+    "peggy": ["jam"],
+    "thom": [],
+    "jonny": []
+}
+
+
+# search_queue += graph["you"]
+# print(search_queue)
+
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    searched = []
+    # print(searched)
+    print(search_queue)
+    while search_queue:
+        person = search_queue.popleft()
+        print(person)
+        # print(searched)
+        print(search_queue)
+        if person not in searched:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+                searched.append(person)
+
+    return False
+
+
+search("you")
